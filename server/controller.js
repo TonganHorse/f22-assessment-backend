ee4640ce5c348966666a895be3c84a077bfd668b
tuli kinikini <tuli.kinikini@gmail.com>
2022-05-09 13:23:48 -0600

    final commit

--- server/controller.js
+++ server/controller.js
@@ -3,8 +3,6 @@ const res = require("express/lib/response");
 const fortunes = ['A beautiful, smart, and loving person will be coming into your life.', 'A dubious friend may be an enemy in camouflage.', 'A faithful friend is a strong defense.', 'A feather in the hand is better than a bird in the air.']
 const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
 
-let users = []
-let globalId = 0
 
 module.exports = {
 
